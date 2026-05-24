1486674714aa773d23fcb9c5123f10311d7951ea
nicholasraynes <nicholasraynes@yahoo.com>
2026-05-24 18:44:47 -0400

    chore: init

---
+++ mocked-up/target/debug/deps/mocked_up-0afd3cd39cc07ce8.d
@@ -0,0 +1,5 @@
+/Users/nicholasraynes/Projects/Coding/mock_workspace/mocked-up/target/debug/deps/mocked_up-0afd3cd39cc07ce8.d: src/lib.rs
+
+/Users/nicholasraynes/Projects/Coding/mock_workspace/mocked-up/target/debug/deps/libmocked_up-0afd3cd39cc07ce8.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ mocked-up/target/debug/deps/mocked_up-4aa427f9ea96eb69.d
@@ -0,0 +1,5 @@
+/Users/nicholasraynes/Projects/Coding/mock_workspace/mocked-up/target/debug/deps/mocked_up-4aa427f9ea96eb69.d: src/lib.rs
+
+/Users/nicholasraynes/Projects/Coding/mock_workspace/mocked-up/target/debug/deps/libmocked_up-4aa427f9ea96eb69.rmeta: src/lib.rs
+
+src/lib.rs:
